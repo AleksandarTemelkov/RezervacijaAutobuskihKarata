98d6343fc20fb5dfbdc2a85480676a0b28a578ff
aleksa <aleksaterzic77@gmail.com>
2023-03-24 03:59:42 +0100

    zavrsen slide show

--- public/js/user-manual.js
+++ public/js/user-manual.js
@@ -1,23 +1,19 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+showSlides();
 
-// Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    showSlides(slideIndex += n);
   }
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
   slides[slideIndex-1].style.display = "block";
+  // Menjanje slike na 5 sec
+  setTimeout(showSlides, 5000); 
 }
